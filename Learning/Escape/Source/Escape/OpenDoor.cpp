22b8220bca65091b5efce43b7a2e9b0fd9b8dcfb
Matt <mattmillergraphics@gmail.com>
2016-12-31 17:16:39 +0000

    door close on delay
    door closes after leaving the specified area - time set to .5s by
    default but can be changed in the editor.

--- Learning/Escape/Source/Escape/OpenDoor.cpp
+++ Learning/Escape/Source/Escape/OpenDoor.cpp
@@ -21,19 +21,24 @@ void UOpenDoor::BeginPlay()
 
 	Super::BeginPlay();
 
+	Owner = GetOwner();
 	ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
-	
+
 }
 
 
 void UOpenDoor::OpenDoor() 
 {
 
-	AActor* Owner = GetOwner();
+	Owner->SetActorRotation(FRotator(0.0f, OpenAngle, 0.0f));
 
-	FRotator NewRotation = FRotator(0.0f, -65.0f, 0.0f);
+}
 
-	Owner->SetActorRotation(NewRotation);
+
+void UOpenDoor::CloseDoor()
+{
+
+	Owner->SetActorRotation(FRotator(0.0f, 0.0f, 0.0f));
 
 }
 
@@ -47,6 +52,13 @@ void UOpenDoor::TickComponent( float DeltaTime, ELevelTick TickType, FActorCompo
 	if (PressurePlate->IsOverlappingActor(ActorThatOpens))
 	{
 		OpenDoor();
+		LastDoorOpenTime = GetWorld()->GetTimeSeconds();
+	}
+
+
+	if (GetWorld()->GetTimeSeconds() - LastDoorOpenTime > DoorCloseDelay) 
+	{
+		CloseDoor();
 	}
 
 }
