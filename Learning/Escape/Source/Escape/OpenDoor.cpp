ceca58f2120e2c3700013e5750b0ab8d15b5eae7
Matt <mattmillergraphics@gmail.com>
2017-01-10 21:31:04 +0000

    Refactor to handle in BP
    Allows us to handle open and close in BluePrint and therefore be able
    make it more flexiable and editable in the editor.

--- Learning/Escape/Source/Escape/OpenDoor.cpp
+++ Learning/Escape/Source/Escape/OpenDoor.cpp
@@ -31,36 +31,18 @@ void UOpenDoor::BeginPlay()
 }
 
 
-void UOpenDoor::OpenDoor() 
-{
-	//Owner->SetActorRotation(FRotator(0.0f, OpenAngle, 0.0f));
-	OnOpenRequest.Broadcast();
-}
-
-
-void UOpenDoor::CloseDoor()
-{
-	Owner->SetActorRotation(FRotator(0.0f, 0.0f, 0.0f));
-}
-
-
 // Called every frame
 void UOpenDoor::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
 {
 	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
 
-
-
-	if (getTotalMassOfActorsOnPlate() > 30.f) //TODO MAKE INTO PARAM
+	if (getTotalMassOfActorsOnPlate() > TriggerMass)
 	{
-		OpenDoor();
-		LastDoorOpenTime = GetWorld()->GetTimeSeconds();
+		OnOpen.Broadcast();
 	}
-
-
-	if (GetWorld()->GetTimeSeconds() - LastDoorOpenTime > DoorCloseDelay) 
+	else
 	{
-		CloseDoor();
+		OnClose.Broadcast();
 	}
 
 }
